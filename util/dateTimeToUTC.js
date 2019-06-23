4fd4862559a122695d4bd6a5eeb8ec8ebe858748
manny martinez <vccengineers12@gmail.com>
2019-06-22 18:06:56 -0700

    removed util.js files

--- util/dateTimeToUTC.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var date_fns_1 = require("date-fns");
-function util_dateTimeToUTC(dateStr) {
-    var _a = dateStr.split(' - ').map(function (val) { return val; }), time = _a[0], date = _a[1];
-    var _b = date.split(' ').map(function (val) { return val; }), day = _b[0], month = _b[1], year = _b[2];
-    return date_fns_1.parse(month + " " + day + " " + year + " " + time).toUTCString();
-}
-exports.util_dateTimeToUTC = util_dateTimeToUTC;
--- util/suffixes.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SUFFIXES = new Set(['I', 'II', 'III', 'IV', 'V', 'Jr', 'Sr']);
