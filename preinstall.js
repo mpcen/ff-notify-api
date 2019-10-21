45e3bc9b7aaeb95b43f278ad4e8f6bf43831bcbe
manny martinez <vccengineers12@gmail.com>
2019-10-20 20:00:58 -0700

    using relative path

--- preinstall.js
+++ preinstall.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const { GOOGLE_CONFIG, GOOGLE_APPLICATION_CREDENTIALS } = process.env;
 
-fs.writeFile(GOOGLE_APPLICATION_CREDENTIALS, GOOGLE_CONFIG, err => {
+fs.writeFile(`${__dirname}/${GOOGLE_APPLICATION_CREDENTIALS}`, GOOGLE_CONFIG, err => {
     if (err) {
         console.log(GOOGLE_APPLICATION_CREDENTIALS, err);
         throw new Error('Error writing', GOOGLE_APPLICATION_CREDENTIALS, err);
