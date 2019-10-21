ce6a4d89b71cb32cecc562e11b38ee8fc578709e
manny martinez <vccengineers12@gmail.com>
2019-10-20 19:47:00 -0700

    logging

--- preinstall.js
+++ preinstall.js
@@ -6,6 +6,7 @@ const { GOOGLE_CONFIG, GOOGLE_APPLICATION_CREDENTIALS } = process.env;
 
 fs.writeFile(GOOGLE_APPLICATION_CREDENTIALS, GOOGLE_CONFIG, err => {
     if (err) {
+        console.log(GOOGLE_APPLICATION_CREDENTIALS, err);
         throw new Error('Error writing', GOOGLE_APPLICATION_CREDENTIALS, err);
     }
 
